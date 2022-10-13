1de1396094e9a6b9877e339f5d6ed92f4d2d5b2b
Nando <nandovga@hotmail.com>
2022-10-13 20:05:40 -0300

    Req Migração - Ajuste deploy

--- resources/views/home/service.blade.php
+++ resources/views/home/service.blade.php
@@ -6,23 +6,23 @@
                 <div class="w-100 d-flex mt-4 flex-wrap">
                     <x-service image="controladoria"
                                title="Controladoria"
-                               link="texte"
+                               link="{{route('imaq')}}"
                                text="Voltado para a Controladoria Municipal o Maquiavel já está de acordo com a nova Contabilidade Pública - PCASP. O sistema também proporciona uma ferramenta ampla para exportar arquivos para a Prestação de Contas."/>
                     <x-service image="almoxarifado"
                                title="Compras e Almoxarifado"
-                               link="texte"
+                               link="{{route('imaq')}}"
                                text="Indicado para gerenciar as compras atendendo as normatizações vigentes, integrado com a Gestão Orçamentária do Município. Em licitações, permite que o gerenciador tenha um maior controle e transparência dos gastos."/>
                     <x-service image="tributario"
                                title="Tributário"
-                               link="texte"
+                               link="{{route('terra')}}"
                                text="Voltado para o Setor Tributário Municipal. Através dele é possível acompanhar cada tributo, desde seu lançamento até a efetiva quitação.."/>
                     <x-service image="lei"
                                title="Legislativo"
-                               link="texte"
+                               link="{{route('legislador')}}"
                                text="Ferramenta Operacional que integra, facilita e agiliza as atividades de Câmaras, Assembleia Legislativa e Prefeituras. É uma ótima opção para reduzir o volume de papéis e organizar todo o arquivo de forma mais prática.."/>
                     <x-service image="rh"
                                title="Gestão de Pessoas"
-                               link="texte"
+                               link="{{route('gestao-rh')}}"
                                text="Ferramenta que auxilia no gerenciamento do pessoal e deixe o empregador atualizado com as mudanças na legislação. Uma solução para as empresas de todos os portes."/>
                 </div>
             </div>
